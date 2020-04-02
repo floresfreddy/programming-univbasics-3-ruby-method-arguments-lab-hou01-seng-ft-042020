199ee2060e0222c05d94bf56fcf59fd24631dd1c
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-02 03:16:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,17 +8,21 @@
 def introduction(name)
   puts "Hi, my name is #{name}"
 end 
-
 introduction(name)
+
+
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}." 
 end 
-
 introduction_with_language
 
+
+
 def introduction_with_language_optional(name, language = "Ruby")
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+introduction_with_language_optional
 
 
 
